680c73ac07cfb51c369a5ffe3bd5b66598cd75ea
wzy1020 <wengzhengyang1020@163.com>
2024-05-28 16:28:22 +0800

    翁正阳5.28-3

--- Equipment_Status/models.py
+++ Equipment_Status/models.py
@@ -5,7 +5,7 @@ from System_Config.models import sensorConfig
 
 # 设备运行状况界面信息
 class machineStatus(models.Model):
-    id = models.ForeignKey(systemConfig, db_constraint=True, null=True, on_delete=models.CASCADE())  # 机床id
+    id = models.CharField(primary_key=True, max_length=32)  # 机床id
     monitor_status = models.BooleanField(default=False)  # 全部开始监测状态
     component_name = models.CharField(max_length=32, null=False)  # 部件名称
     component_number = models.CharField(max_length=32, null=False)  # 部件编号
@@ -14,14 +14,14 @@ class machineStatus(models.Model):
 
 
 class temperaturePowerAcceleration(models.Model):
-    id = models.ForeignKey(systemConfig, db_constraint=True, null=True, on_delete=models.CASCADE())  # 机床id
+    id = models.CharField(primary_key=True, max_length=32)  # 机床id
     machine_temperature = models.CharField(max_length=32, null=False)  # 机床运行温度
     machine_power = models.CharField(max_length=32, null=False)  # 机床运行功率
     machine_acceleration = models.CharField(max_length=32, null=False)  # 机床主轴加速度
 
 
 class warningWithFaultCode(models.Model):
-    id = models.ForeignKey(systemConfig, db_constraint=True, null=True, on_delete=models.CASCADE())  # 机床id
+    id = models.CharField(primary_key=True, max_length=32)  # 机床id
     warning_time = models.CharField(max_length=32, null=False)  # 警告时间
     warning_component = models.CharField(max_length=32, null=False)  # 警告部件
     fault_type = models.CharField(max_length=32, null=False)  # 报警类型
@@ -29,11 +29,11 @@ class warningWithFaultCode(models.Model):
 
 
 class thermalDiagram(models.Model):
-    id = models.ForeignKey(systemConfig, db_constraint=True, null=True, on_delete=models.CASCADE())  # 机床id
+    id = models.CharField(primary_key=True, max_length=32)  # 机床id
     machine_running_time = models.CharField(max_length=32, null=False)  # 机床加工时间
 
 
 class sensorRunningCondition(models.Model):
-    id = models.ForeignKey(sensorConfig, db_constraint=True, null=True, on_delete=models.CASCADE())  # 传感器id
+    id = models.CharField(primary_key=True, max_length=32)  # 传感器id
     sensor_name = models.CharField(max_length=32, null=False)  # 传感器名称
     sensor_status = models.CharField(max_length=32, null=False)  # 传感器运行状况
