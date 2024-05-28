76e101575527656cf8549a613c55bb7828955488
heiyo2333 <2431461804@qq.com>
2024-05-28 17:35:09 +0800

    新增配置

--- System_Config/serializer.py
+++ System_Config/serializer.py
@@ -2,65 +2,19 @@ from rest_framework import serializers
 
 
 
-# 配置信息响应
-class ConfigInformationSerializer(serializers.Serializer):
-    machine_ip = serializers.CharField(label="机床IP")
-    machine_port = serializers.IntegerField(label="端口号")
-    tool_number = serializers.IntegerField(label="刀位数量")
-    vib_serial_port = serializers.CharField(label="振动传感器串口")
-    cur_serial_port = serializers.CharField(label="电流传感器串口")
-    monitor_channel_data = serializers.CharField(label="监控配置数据")
-    alarm_cancel_method = serializers.CharField(label="报警取消方式，0：延时取消，1：IO输入取消")
-    delay_cancel = serializers.IntegerField(label="延时取消-延时")
-    io_input_cancel_port = serializers.IntegerField(label="IO输入取消-端口")
-    alarm_data_delay1 = serializers.IntegerField(label="报警数据正延时")
-    alarm_data_delay2 = serializers.IntegerField(label="报警数据负延时")
-    data_save_method = serializers.CharField(label="数据保存方式，0：不保存，1：连续保存，2：分时段保存，3：IO触发保存")
-    data_save_data = serializers.CharField(label="分时段保存——数据")
-    io_input_save_port = serializers.IntegerField(label="IO触发保存端口")
-    self_starting = serializers.IntegerField(label="自启动，0：否，1：是")
-
-
-# 修改配置请求
-class ConfigUpdateSerializer(serializers.Serializer):
-    config_id = serializers.IntegerField(help_text="系统配置id", )
-    machine_ip = serializers.CharField(help_text="机床IP")
-    machine_port = serializers.IntegerField(help_text="端口号")
-    tool_number = serializers.IntegerField(help_text="刀位数量")
-    vib_serial_port = serializers.CharField(help_text="振动传感器串口")
-    cur_serial_port = serializers.CharField(help_text="电流传感器串口")
-    monitor_channel_data = serializers.CharField(help_text="监控配置数据")
-    alarm_cancel_method = serializers.CharField(help_text="报警取消方式，0：延时取消，1：IO输入取消")
-    delay_cancel = serializers.IntegerField(required=False, help_text="延时取消-延时")
-    io_input_cancel_port = serializers.IntegerField(required=False, help_text="IO输入取消-端口")
-    alarm_data_delay1 = serializers.IntegerField(help_text="报警数据正延时")
-    alarm_data_delay2 = serializers.IntegerField(help_text="报警数据负延时")
-    data_save_method = serializers.CharField(help_text="数据保存方式，0：不保存，1：连续保存，2：分时段保存，3：IO触发保存")
-    data_save_data = serializers.CharField(required=False, help_text="分时段保存——数据")
-    io_input_save_port = serializers.IntegerField(required=False, help_text="IO触发保存端口")
-    self_starting = serializers.IntegerField(help_text="自启动，0：否，1：是")
-    home_monitor_number = serializers.IntegerField(help_text="首页信号显示数量")
-    learning_monitor_number = serializers.IntegerField(help_text="自适应学习信号显示数量")
-    failure_monitor_number = serializers.IntegerField(help_text="异常监测信号显示数量")
 
 
 # 新增配置请求
 class ConfigAddSerializer(serializers.Serializer):
-    machine_ip = serializers.CharField(help_text="机床IP")
+    id = serializers.CharField(help_text="ID")
+    machine_name = serializers.CharField(help_text="机床名称")
+    machine_type = serializers.CharField(help_text="机床型号")
+    machine_description = serializers.CharField(help_text="机床描述")
+    manager = serializers.CharField(help_text="负责人")
+    machine_ip = serializers.CharField(help_text="机床ip")
     machine_port = serializers.IntegerField(help_text="端口号")
     tool_number = serializers.IntegerField(help_text="刀位数量")
-    vib_serial_port = serializers.CharField(help_text="振动传感器串口")
-    cur_serial_port = serializers.CharField(help_text="电流传感器串口")
-    monitor_channel_data = serializers.CharField(help_text="监控配置数据")
-    alarm_cancel_method = serializers.CharField(help_text="报警取消方式，0：延时取消，1：IO输入取消")
-    delay_cancel = serializers.IntegerField(required=False, help_text="延时取消-延时")
-    io_input_cancel_port = serializers.IntegerField(required=False, help_text="IO输入取消-端口")
-    alarm_data_delay1 = serializers.IntegerField(help_text="报警数据正延时")
-    alarm_data_delay2 = serializers.IntegerField(help_text="报警数据负延时")
-    data_save_method = serializers.CharField(help_text="数据保存方式，0：不保存，1：连续保存，2：分时段保存，3：IO触发保存")
-    data_save_data = serializers.CharField(required=False, help_text="分时段保存——数据")
-    io_input_save_port = serializers.IntegerField(required=False, help_text="IO触发保存端口")
-    self_starting = serializers.IntegerField(help_text="自启动，0：否，1：是")
-    home_monitor_number = serializers.IntegerField(help_text="首页信号显示数量")
-    learning_monitor_number = serializers.IntegerField(help_text="自适应学习信号显示数量")
-    failure_monitor_number = serializers.IntegerField(help_text="异常监测信号显示数量")
+    database_name = serializers.CharField(help_text="时序数据库名称")
+    alarm_data_delay_positive = serializers.IntegerField(help_text="正延时")
+    alarm_data_delay_negative = serializers.IntegerField(help_text="负延时")
+
--- System_Config/views.py
+++ System_Config/views.py
@@ -8,7 +8,7 @@ from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
 from rest_framework.decorators import action
 
-from . import models
+from . import models, serializer
 
 
 class SystemConfigViewSet(viewsets.GenericViewSet):
@@ -18,3 +18,40 @@ class SystemConfigViewSet(viewsets.GenericViewSet):
     # 返回数据库所有对象
     def get_queryset(self):
         return models.SystemConfiguration.objects.all()
+
+    # 新增配置
+    @swagger_auto_schema(
+        operation_summary='新增配置',
+        request_body=serializer.ConfigAddSerializer,
+        responses={200: openapi.Response('successful', serializer.ConfigAddSerializer)},
+        tags=["config"],
+    )
+    @action(detail=False, methods=['post'])
+    def add(self, request):
+        id = self.request.data.get('id')
+        machine_name = self.request.data.get('machine_name')
+        machine_type = self.request.data.get('machine_type')
+        machine_description = self.request.data.get('machine_description')
+        manager = self.request.data.get('manager')
+        machine_ip = self.request.data.get('machine_ip')
+        machine_port = self.request.data.get('machine_port')
+        tool_number = self.request.data.get('tool_number')
+        database_name = self.request.data.get('database_name')
+        alarm_data_delay_positive = self.request.data.get('alarm_data_delay_positive')
+        alarm_data_delay_negative = self.request.data.get('alarm_data_delay_negative')
+        models.systemConfig.objects.create(id=id,
+                                           machine_name=machine_name,
+                                           machine_type=machine_type,
+                                           machine_description=machine_description,
+                                           manager=manager,
+                                           machine_ip=machine_ip,
+                                           machine_port=machine_port,
+                                           tool_number=tool_number,
+                                           database_name=database_name,
+                                           alarm_data_delay_positive=alarm_data_delay_positive,
+                                           alarm_data_delay_negative=alarm_data_delay_negative, )
+        response = {
+            'status': 200,
+            'message': '新增配置成功'
+        }
+        return JsonResponse(response)
