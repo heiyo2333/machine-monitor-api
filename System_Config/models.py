8ddb00399f4d997c5d6294b97e9cf0d4ef10b947
heiyo2333 <2431461804@qq.com>
2024-05-30 21:54:52 +0800

    殷爽5-30-2

--- System_Config/models.py
+++ System_Config/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+
+
 # 机床配置
 class systemConfig(models.Model):
-    # id = models.IntegerField(primary_key=True)  # id
     machine_code = models.CharField(max_length=32, null=True)  # 机床编号
     machine_name = models.CharField(max_length=32, null=True)  # 机床名称
     machine_type = models.CharField(max_length=32, null=True)  # 机床型号
@@ -15,19 +16,19 @@ class systemConfig(models.Model):
     alarm_data_delay_negative = models.IntegerField(null=True)  # 负延时
     is_apply = models.BooleanField(default=0)  # 应用配置
 
+
 # 传感器配置
 class sensorConfig(models.Model):
     sensor_code = models.CharField(max_length=32)  # 传感器编号
     sensor_name = models.CharField(max_length=32, null=True)  # 传感器名称
     frequency = models.IntegerField(null=True)  # 采样频率
     channel_number = models.IntegerField(null=True)  # 通道数
-    remark=models.CharField(max_length=32,null=True)# 备注
+    remark = models.CharField(max_length=32, null=True)  # 备注
     sensor_status = models.BooleanField(default=False)  # 状态
 
 
 # 通道配置
 class channelConfig(models.Model):
-
     sensor_name = models.CharField(max_length=32, null=True)  # 传感器名称
     channel_name = models.CharField(null=True, max_length=32)  # 通道名称
     overrun_times = models.IntegerField(null=True)  # 超限次数
@@ -39,6 +40,3 @@ class channelConfig(models.Model):
 #     name = models.CharField(max_length=4, null=True)  # 名称
 #     value = models.CharField(max_length=4, null=True)  # 值
 #     type = models.CharField(max_length=4, null=True)  # 类型
-
-
-
