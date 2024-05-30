d48129110ec7c6e2b66146d59e29deab8e5aa05a
wzy1020 <wengzhengyang1020@163.com>
2024-05-30 18:18:49 +0800

    翁正阳5-30-3

--- Method_Config/serializer.py
+++ Method_Config/serializer.py
@@ -21,8 +21,22 @@ class deleteComponentConfigSerializer(serializers.Serializer):
     machine_code = serializers.CharField(label="机床编号", max_length=32)  # 机床编号
 
 
-# 部件配置：新增部件请求序列化器、编辑部件请求序列化器
+# 部件配置：新增部件请求序列化器
 class componentConfigSerializer(serializers.Serializer):
+    config_id = serializers.IntegerField(label="系统配置id")  # (实际上是系统配置的id)
+    component_name = serializers.CharField(label="部件名称", max_length=32)  # 部件名称
+    algorithm_id = serializers.IntegerField(label="算法id", required=True)  # 算法id
+    input_channel1 = serializers.IntegerField(label="算法输入通道1", required=False)  # 算法输入通道1对应的id
+    input_channel2 = serializers.IntegerField(label="算法输入通道2", required=False)  # 算法输入通道2对应的id
+    input_channel3 = serializers.IntegerField(label="算法输入通道3", required=False)  # 算法输入通道3对应的id
+    input_channel4 = serializers.IntegerField(label="算法输入通道4", required=False)  # 算法输入通道4对应的id
+    input_channel5 = serializers.IntegerField(label="算法输入通道5", required=False)  # 算法输入通道5对应的id
+    remark = serializers.CharField(label="备注", max_length=32, required=False)  # 备注
+
+
+# 部件配置：编辑部件请求序列化器
+class changeComponentConfigSerializer(serializers.Serializer):
+    id = serializers.IntegerField(label="编辑配置表id")
     config_id = serializers.IntegerField(label="系统配置id")  # (实际上是系统配置的id)
     component_name = serializers.CharField(label="部件名称", max_length=32)  # 部件名称
     algorithm_id = serializers.IntegerField(label="算法id", required=True)  # 算法id
